a9b6b982cc039e1d2184d054f67df7bf8418a614
JohanTeacher <johan.liewendahl@fryshuset.se>
2017-05-17 10:34:59 +0200

    Kameran
    Följer spelaren korrekt med en osynlig ruta i mitten som man kan röra
    sig fritt inom utan att kameran påverkas.
    
    Nästa steg återstår dock; att göra så att kameran inte kan se utanför
    banans gränser.

--- Assets/Scripts/CameraCollisionScript.cs
+++ Assets/Scripts/CameraCollisionScript.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class CameraCollisionScript : MonoBehaviour {
 
 	public GameObject maincharacter;
+	public GameObject levelLimits;
+	public Canvas UICanvas;
+	public Camera mainCamera;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,47 +18,116 @@ public class CameraCollisionScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		//Get bouds to check collision of camera view collider
+		//Get bouds to check collisions 
 		Bounds thisBounds = this.GetComponent<Collider2D> ().bounds;
 		Bounds mainCharBounds = maincharacter.GetComponent<Collider2D> ().bounds;
+		Bounds levelLimitsBounds = levelLimits.GetComponent<BoxCollider2D>().bounds;
 
-		//New position for the camera
+		//Camera bounds is something extra :)
+		//mainCamera.transform.position
+		//mainCamera.orthographicSize
+		Vector3 cameraCenter = mainCamera.transform.position;
+		Vector3 cameraSize = new Vector3(Screen.width, Screen.height); //whhaaaat? 
+		Bounds cameraBounds = new Bounds(cameraCenter,cameraSize);
+
+
+		//Calculate camera view collider
+
+		//New position for the camera trigger box (smal box around mainchar)
 		float newX = this.transform.position.x; 
-		float newY = maincharacter.transform.position.y;
+		float newY = this.transform.position.y; 
 
-		if(!thisBounds.Intersects(mainCharBounds)){
-			//mainchar is outside of the collider box!!
+		//canvas.transform.position
+		//canvas.pixelRect.width/2
 
-			//On what side of the box?
-			if (maincharacter.transform.position.x < this.transform.position.x - (this.GetComponent<Collider2D> ().bounds.size.x / 2)) {
-				//mainchars' on the LEFT side of collider
-				newX = maincharacter.transform.position.x + (this.GetComponent<Collider2D> ().bounds.size.x / 2) + (maincharacter.GetComponent<SpriteRenderer> ().bounds.size.x/2.2f);
-			
-			} 
-			else if (maincharacter.transform.position.x > this.transform.position.x - (this.GetComponent<Collider2D> ().bounds.size.x / 2)) {
-				//mainchar's on the RIGHT side of collider
-				newX = maincharacter.transform.position.x - (this.GetComponent<Collider2D> ().bounds.size.x / 2) - (maincharacter.GetComponent<SpriteRenderer> ().bounds.size.x/1.7f);
-			
-			}
-			/*if (maincharacter.transform.position.y > this.transform.position.y - (this.GetComponent<Collider2D> ().bounds.size.y / 2)) {
-				//mainchar's OVER the collider
-				//newY = maincharacter.transform.position.y - (this.GetComponent<Collider2D> ().bounds.size.y / 2) - (maincharacter.GetComponent<SpriteRenderer> ().bounds.size.y/2);
-			
-			} 
-			else if (maincharacter.transform.position.y < this.transform.position.y - (this.GetComponent<Collider2D> ().bounds.size.y / 2)) {
-				//mainchar is UNDER the collider
-				//newY = maincharacter.transform.position.y + (this.GetComponent<Collider2D> ().bounds.size.y / 2) + (maincharacter.GetComponent<SpriteRenderer> ().bounds.size.y/2);
-			
-			}*/
-
+		if (LeftBoundsCollision (mainCharBounds, thisBounds)) {
+			newX = maincharacter.transform.position.x + (thisBounds.size.x/2 - mainCharBounds.size.x/2);
+		}
+		else if (RightBoundsCollision (mainCharBounds, thisBounds)) {
+			newX = maincharacter.transform.position.x - (thisBounds.size.x/2 - mainCharBounds.size.x/2)+0.1f;
 		}
 
-		//Move the camera to folow the mainchar
+		if (UpBoundsCollision (mainCharBounds, thisBounds)) {
+			newY = mainCharBounds.center.y - (thisBounds.size.y/2 - mainCharBounds.size.y/2)+0.1f; //WHYYYYYYYY!!?!?!
+		}
+		else if (DownBoundsCollision (mainCharBounds, thisBounds)) {
+			newY = mainCharBounds.center.y + (thisBounds.size.y/2 - mainCharBounds.size.y/2);
+		}
+			
+		//Move the collision box to folow the mainchar
 		this.transform.position = new Vector2 (newX, newY);
 
+		//Move the camera to follow the collision box
+		mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, mainCamera.transform.position.z);
+
+		/*if (LeftBoundsCollision (cameraBounds, levelLimitsBounds)) {
+			print ("LEFT");
+		}*/
 	}
 
+	Bounds setCameraBounds()
+	{
+		Vector3 cameraCenter = mainCamera.transform.position;
+		Vector3 cameraSize = new Vector3(Screen.width, Screen.height); //whhaaaat?  
 
+		return new Bounds(cameraCenter,cameraSize);
+	}
+
+	bool LeftBoundsCollision(Bounds first, Bounds second)
+	{
+		//Returnes true if first bounds is enetering left wall of second bounds
+
+		float firstLeftPositionX = first.center.x - (first.size.x / 2);
+		float secondLeftPositionX = second.center.x - (second.size.x / 2);
+
+		if (firstLeftPositionX <= secondLeftPositionX) {
+			return true;
+		}
+
+		return false;
+	}
+
+	bool RightBoundsCollision(Bounds first, Bounds second)
+	{
+		//Returnes true if first bounds is enetering right wall of second bounds
+
+		float firstRightPositionX = first.center.x + (first.size.x / 2);
+		float secondRightPositionX = second.center.x + (second.size.x / 2);
+
+		if (firstRightPositionX >= secondRightPositionX) {
+			return true;
+		}
+
+		return false;
+	}
+
+	bool UpBoundsCollision(Bounds first, Bounds second)
+	{
+		//Returnes true if first bounds is enetering upper roof of second bounds
+
+		float firstUpPositionY = first.center.y + (first.size.y / 2);
+		float secondUpPositionY = second.center.y + (second.size.y / 2);
+
+		if (firstUpPositionY >= secondUpPositionY) {
+			return true;
+		}
+
+		return false;
+	}
+
+	bool DownBoundsCollision(Bounds first, Bounds second)
+	{
+		//Returnes true if first bounds is enetering lower floor of second bounds
+
+		float firstDownPositionY = first.center.y - (first.size.y / 2);
+		float secondDownPositionY = second.center.y - (second.size.y / 2);
+
+		if (firstDownPositionY <= secondDownPositionY) {
+			return true;
+		}
+
+		return false;
+	}
 	void OnTriggerExit2D(Collider2D other)
 	{
 		if (other.gameObject == maincharacter) {
